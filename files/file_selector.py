3db3912944a34ac0125bf6d7039779e78c34767b
zionhigt <larcher501@gmail.com>
2022-10-30 08:22:29 +0100

    Module files

---
+++ files/file_selector.py
@@ -0,0 +1,17 @@
+import pathlib
+import os
+
+def select(root):
+    res = []
+    dir_ = pathlib.Path(root)
+    for d in dir_.iterdir():
+        if d.is_file():
+            res.append(d)
+    return res
+
+def srcToDest(src, dest, ext_out):
+    file_name = os.path.splitext(os.path.split(src)[1])[0]
+    return os.path.join(dest, file_name + ext_out)
+
+def getExtention(path):
+    return os.path.splitext(os.path.split(path)[1])[1]
